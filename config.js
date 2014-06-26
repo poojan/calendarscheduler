e6617490d0051b8b33aa3ac627d8691e4813c7a4
Poojan Shrestha <poojans@gmail.com>
2014-06-26 14:29:07 +0545

    Changed config.

--- config.js
+++ config.js
@@ -6,15 +6,23 @@ if (!process.env.OPENSHIFT_NODEJS_IP) {
   dbUser = null;
   dbPass = null;
   dbName = 'calendar_scheduler';
-  mongoUri = 'mongodb://localhost/calendar_scheduler';
+  //mongoUri = 'mongodb://localhost/calendar_scheduler';
 } else {
-  dbHost = $OPENSHIFT_MONGODB_DB_HOST;
-  dbPort = $OPENSHIFT_MONGODB_DB_PORT;
+  //dbHost = $OPENSHIFT_MONGODB_DB_HOST;
+  //dbPort = $OPENSHIFT_MONGODB_DB_PORT;
+  dbHost = process.env.OPENSHIFT_MONGODB_DB_HOST;
+  dbPort = process.env.OPENSHIFT_MONGODB_DB_PORT;
   dbUser = 'admin';
   dbPass = '5ePjWTMZljww';
   dbName = 'calendarscheduler';
-  mongoUri = 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/';
+  //mongoUri = 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/';
+
+  console.log('process.env.OPENSHIFT_MONGODB_DB_HOST', process.env.OPENSHIFT_MONGODB_DB_HOST);
+  console.log('process.env.$OPENSHIFT_MONGODB_DB_HOST', process.env.$OPENSHIFT_MONGODB_DB_HOST);
+  console.log('$OPENSHIFT_MONGODB_DB_HOST', $OPENSHIFT_MONGODB_DB_HOST);
 }
+
+mongoUri = 'mongodb://' + dbHost + ':' + dbPort + '/' + dbName;
 //var mongoUri = 'mongodb://' + dbUser + ':' + dbPass + '@' + '$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/' + dbName;
 //console.log('mongoUri', mongoUri);
 
