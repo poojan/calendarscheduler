c9a203514f7efac06623f82190d4a4fda551e0bb
Poojan Shrestha <poojans@gmail.com>
2014-07-08 16:55:04 +0545

    Added activeNav directive.

--- views/js/directives.js
+++ views/js/directives.js
@@ -35,3 +35,37 @@ angular.module('CSApp')
       }
     };
   });
+
+angular.module('CSApp')
+  .directive('activeNav', function ($location) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var anchor = element[0];
+        if (element[0].tagName.toUpperCase() != 'A') {
+          anchor = element.find('a')[0];
+        }
+        var path = anchor.href;
+
+        scope.location = $location;
+        scope.$watch('location.absUrl()', function (newPath) {
+          path = normalizeUrl(path);
+          newPath = normalizeUrl(newPath);
+
+          if (path === newPath ||
+             (attrs.activeNav === 'nestedTop' && newPath.indexOf(path) === 0)) {
+            element.addClass('active');
+          } else {
+            element.removeClass('active');
+          }
+        });
+      }
+    };
+
+    function normalizeUrl(url) {
+      if (url[url.length - 1] !== '/') {
+        url = url + '/';
+      }
+      return url;
+    }
+  });
